bb13dc1740e68773d5d44e5b426029868506b454
Your Name <email@example.com>
2020-04-13 17:45:24 +0800

    更新3

--- case/test_search.py
+++ case/test_search.py
@@ -9,7 +9,7 @@ from page.page_factory import PageFactory
 from utils import get_driver
 
 
-class TestSearch(object):
+class TestSearchc(object):
     """搜索测试类"""
 
     @pytest.fixture(autouse=True)
